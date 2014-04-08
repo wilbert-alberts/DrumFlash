d2f45eeda3f001bb4482942fa8abce190a465b23
ben <ben@ben-kubuntu-11.10>
2014-04-08 21:57:12 +0200

    Iron out remaining bugs.

--- DrumFlash/main.c
+++ DrumFlash/main.c
@@ -20,12 +20,11 @@
 // gnd                    (4)
 // vcc                    (8)
 
-#define CLOCKFREQUENCY (1000000)
-#define PRESCALER      (1024)
+#define CLOCKFREQUENCY (1000000L)
+#define PRESCALER      (4096)
 
-#define FLASHDURATION (100)
-#define DEAFDURATION  (400)
-
+#define FLASHDURATION (10)
+#define DEAFDURATION  (190)
 #define DBG_STARTED   (0)
 #define DBG_IDLE      (1)
 #define DBG_TRIGGERED (2)
@@ -53,8 +52,10 @@ TimerCB wakeupCB = 0;
 
 int main()
 {
-	debugOut(DBG_STARTED);
+//	debugOut(DBG_STARTED);
 	initialize();
+
+
 	handleSoundHeard();
 	halt();
 }
@@ -62,7 +63,15 @@ int main()
 void halt()
 {
 	// Todo: look for a better way to halt.
-	while (1);
+	while (1) {
+      uint8_t  v = ACSR & _BV(ACO);
+      if (v==0) {
+         PORTB &= ~_BV(PORTB4);
+      }
+      else {
+         PORTB |= _BV(PORTB4);
+      }
+   }
 }
 
 ISR(TIMER1_COMPA_vect)
@@ -71,7 +80,7 @@ ISR(TIMER1_COMPA_vect)
 	TCCR1 &= ~(_BV(CS13) |_BV(CS12) |_BV(CS11) |_BV(CS10));
 
 	// Invoke registered callback
-	if (wakeupCB==0)
+	if (wakeupCB!=0)
 		wakeupCB();
 }
 
@@ -84,23 +93,29 @@ void initialize()
 {
 	cli();
 
+
 	// Set clock prescaler such that we run on 1 Mhz
 	CLKPR = _BV(CLKPCE);
 	CLKPR = _BV (CLKPS1) | _BV(CLKPS0);
 
-	// Set light output (PB4) to output
+	// Set light output (PB3) to output
 	// (default all pins are set to output
+	DDRB  |= _BV(DDB3);
+
+   // Set debug ports to output
+	DDRB  |= _BV(DDB4);
+
 
 	// Set sound inputs to input
 	// Turn off pull up resistor
 	// sound reference  AIN1  (6)
 	// sound input      AIN0  (5)
-	DDRB  &= ~(_BV(DDB1)|_BV(DDB0));
-	PORTB &= ~(_BV(PORTB1)|_BV(PORTB0));
+	DDRB  &= ~(_BV(DDB1) | _BV(DDB0));
+	PORTB &= ~(_BV(PORTB1) | _BV(PORTB0));
 
 	// Set comparator to trigger on flank.
 	ACSR &= ~_BV(ACIE);
-	ACSR = (ACSR & ~_BV(ACIS1)) | _BV(ACIS0);
+	ACSR &= ~(_BV(ACIS1) | _BV(ACIS0));
 
 	sei();
 }
@@ -108,7 +123,10 @@ void initialize()
 void enableInputEvent()
 {
 	// enable pin driven interrupt
-	ACSR |= _BV(ACIE);
+   cli();
+	ACSR |= _BV(ACIE) | _BV(ACIS1) | _BV(ACIS0);
+   ACSR &= ~_BV(ACI);
+   sei();
 	debugOut(DBG_IDLE);
 }
 
@@ -116,7 +134,7 @@ void enableInputEvent()
 void disableInputEvent()
 {
 	// Disable pin driven interrupt
-	ACSR = (ACSR & ~_BV(ACIE)) | _BV(ACD);
+	ACSR &= ~_BV(ACIE);
 }
 
 void activateTimer(int ms, TimerCB cb)
@@ -124,13 +142,13 @@ void activateTimer(int ms, TimerCB cb)
 	wakeupCB = cb;
 
 	// Calculate timer compare value
-	uint8_t cmp = CLOCKFREQUENCY/PRESCALER/1000 * ms;
+	uint8_t cmp = ms * CLOCKFREQUENCY/PRESCALER/1000;
 
 	// Set compare value and start timer
 	OCR1A = cmp;
 	TCNT1 = 0;
 	TIMSK |= _BV(OCIE1A);
-	TCCR1 |= _BV(CTC1) | _BV(CS13) |_BV(CS12) |_BV(CS11) |_BV(CS10);
+	TCCR1 |= _BV(CTC1) | _BV(CS13) |_BV(CS12) /*|_BV(CS11)*/ |_BV(CS10);
 }
 
 
@@ -170,6 +188,7 @@ void lightOff()
 
 void debugOut(int val)
 {
+   return;
 	// debug0           PB2   (7)
 	// debug1           PB4   (3)
 
